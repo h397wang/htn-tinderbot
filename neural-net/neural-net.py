3271f3267a95bf464b3b39cb7432ebcfc6e41144
Rich143 <richard.arnold.matthews@gmail.com>
2016-09-17 23:22:52 -0400

    It works now

--- neural-net/neural-net.py
+++ neural-net/neural-net.py
@@ -12,12 +12,12 @@ import theano.tensor as T
 import lasagne
 import random
 
-dataset_path = '../tinder-bot/dataset/'
+dataset_path = '../../tinder-bot/dataset/'
 pickle_dataset = dataset_path+'dataset.pickle'
 
 image = 'result/female/Zoe_Ball_0001.jpg'
 
-PIXELS = 20
+PIXELS = 28
 
 def load_dataset():
     with open(pickle_dataset, 'rb') as f:
@@ -40,6 +40,8 @@ def load_dataset():
     val_images = dataset[num_train:num_train+num_val-1]
     test_images = dataset[num_train+num_val:]
 
+    positive = 0
+    negative = 0
     i = 0
     for image in train_images:
         img = Image.open(dataset_path+image['path'])
@@ -50,6 +52,10 @@ def load_dataset():
 
         X_train[i][0] = img
         Y_train[i] = int(image['preference'])
+        if Y_train[i] == 1:
+            positive +=1
+        else:
+            negative +=1
         i+=1
 
     i = 0
@@ -83,18 +89,50 @@ def load_dataset():
         i+=1
 
 
+    print("Positives: {}, Negatives {}".format(positive, negative))
     return (X_train, Y_train, X_val, Y_val, X_test, Y_test)
 
 
 def build_mlp(input_var=None):
     print("Building network ...")
     l_in = lasagne.layers.InputLayer(shape=(None, 1, PIXELS, PIXELS), input_var=input_var)
-    l_hid = lasagne.layers.DenseLayer(
-        l_in, num_units=10,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
+    # l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.2)
+    # l_hid = lasagne.layers.DenseLayer(
+        # l_in, num_units=800,
+        # nonlinearity=lasagne.nonlinearities.sigmoid,
+        # W=lasagne.init.Normal())
+
+    # Apply 20% dropout to the input data:
+    l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.2)
+
+    # Add a fully-connected layer of 800 units, using the linear rectifier, and
+    # initializing weights with Glorot's scheme (which is the default anyway):
+    l_hid1 = lasagne.layers.DenseLayer(
+            l_in_drop, num_units=800,
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    # We'll now add dropout of 50%:
+    l_hid1_drop = lasagne.layers.DropoutLayer(l_hid1, p=0.5)
+
+    # Another 800-unit layer:
+    l_hid2 = lasagne.layers.DenseLayer(
+            l_hid1_drop, num_units=800,
+            nonlinearity=lasagne.nonlinearities.rectify)
+
+    # We'll now add dropout of 50%:
+    l_hid2_drop = lasagne.layers.DropoutLayer(l_hid2, p=0.3)
+
+    # Another 800-unit layer:
+    l_hid3 = lasagne.layers.DenseLayer(
+            l_hid2_drop, num_units=100,
+            nonlinearity=lasagne.nonlinearities.sigmoid)
+
+    # 50% dropout again:
+    l_hid3_drop = lasagne.layers.DropoutLayer(l_hid3, p=0.5)
+
     l_out = lasagne.layers.DenseLayer(
-            l_hid, num_units=2,
+            l_hid3_drop, num_units=2,
             nonlinearity=lasagne.nonlinearities.softmax)
 
     # l_in = lasagne.layers.InputLayer(shape=(None, 1, PIXELS, PIXELS), input_var=input_var)
@@ -121,7 +159,7 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
             excerpt = slice(start_idx, start_idx + batchsize)
         yield inputs[excerpt], targets[excerpt]
 
-def main(num_epochs=5):
+def main(num_epochs=50):
      # Load the dataset
     print("Loading data...")
     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
@@ -147,8 +185,10 @@ def main(num_epochs=5):
     # parameters at each training step. Here, we'll use Stochastic Gradient
     # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
     params = lasagne.layers.get_all_params(network, trainable=True)
-    updates = lasagne.updates.sgd(
-            loss, params, learning_rate=1)
+    updates = lasagne.updates.nesterov_momentum(
+            loss, params, learning_rate=0.01, momentum=0.9)
+    # updates = lasagne.updates.sgd(
+            # loss, params, learning_rate=1)
 
     # train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
@@ -181,11 +221,18 @@ def main(num_epochs=5):
     print("Starting training...")
     # We iterate over epochs:
     for epoch in range(num_epochs):
+
+        params = lasagne.layers.get_all_param_values(network)
+
+        # np.set_printoptions(precision=3, threshold=np.inf)
+        # for thing in params:
+            # print(thing)
+
         # In each epoch, we do a full pass over the training data:
         train_err = 0
         train_batches = 0
         start_time = time.time()
-        for batch in iterate_minibatches(X_train, y_train, 50, shuffle=True):
+        for batch in iterate_minibatches(X_train, y_train, 25, shuffle=True):
             inputs, targets = batch
             # print(inputs.shape)
             # print(targets.shape)
@@ -216,6 +263,9 @@ def main(num_epochs=5):
         print("  validation accuracy:\t\t{:.2f} %".format(
             val_acc / val_batches * 100))
 
+        # if (val_acc / val_batches * 100) > 80:
+            # break
+
     # After training, we compute and print the test error:
     test_err = 0
     test_acc = 0
@@ -238,21 +288,21 @@ def main(num_epochs=5):
     for i in range(len(X_test)):
         X_val = X_test[i:i+1]
         # print(X_val[0][0].shape)
-        print(X_val[0][0])
+        # print(X_val[0][0])
         y_val = y_test[i:i+1]
 
         y_predicted = predict_fn(X_val)
 
-        print("predicted {}, actual {}".format(y_predicted, y_val))
+        # print("predicted {}, actual {}".format(y_predicted, y_val))
         print("predicted {}, actual {}".format(np.argmax(y_predicted, axis=1), y_val))
 
-    params = lasagne.layers.get_all_param_values(network)
-    print("Params: {}".format(params))
-    print(type(params))
+    # params = lasagne.layers.get_all_param_values(network)
+    # # print("Params: {}".format(params))
+    # # print(type(params))
 
-    np.set_printoptions(precision=3, threshold=np.inf)
-    for thing in params:
-        print(thing)
+    # np.set_printoptions(precision=3, threshold=np.inf)
+    # for thing in params:
+        # print(thing)
 
     # Optionally, you could now dump the network weights to a file like this:
     # np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
